27d6b5fe44470941388adb15ed88b39f35464369
miguelandres <miguelandres@users.noreply.github.com>
2021-10-13 03:36:04 +0000

    deploy: 6aef678ffed9f90f0f9445c50e1ff767565f62f1

---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["filesystem"] = {"name":"","dirs":[{"name":"fake","files":["mod.rs","node.rs","registry.rs","tempdir.rs"]}],"files":["lib.rs","os.rs"]};
+createSourceSidebar();
